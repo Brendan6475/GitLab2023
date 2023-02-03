0795aa1c597693468dc432a6d15a6af65e04a22a
Brendan6475 <bshanks9@huskers.unl.edu>
2023-02-03 13:03:05 -0600

    made string into array

--- src/edu/unl/raikes/git/GitLab.java
+++ src/edu/unl/raikes/git/GitLab.java
@@ -2,7 +2,7 @@ package edu.unl.raikes.git;
 
 public class GitLab {
     public static void main(String[] args) {
-        GitLabHelper helper = new GitLabHelper("Brendan", "Reed");
+        GitLabHelper helper = new GitLabHelper(new String[] {"person1, person2"});
         helper.PrintWelcomeMessage();
     }
 }
